2cf4522774806d1c09a4770ed1a75648444cb7bf
evvvritt <evv.info@gmail.com>
2017-09-10 13:11:02 -0500

    use live prismic repo

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   PRISMIC: {
-    API: '"https://arpa-dev.prismic.io/api/v2"' 
+    API: '"https://arpa.prismic.io/api/v2"' 
   },
   FLICKER: true
 }
